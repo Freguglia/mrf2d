6e96734a14d13c95407b99446fac6293fb9019c6
Freguglia <victorfreguglia@gmail.com>
2019-06-17 15:40:53 -0300

    Added tests to polynomial basis function.

--- tests/testthat/test-basis_functions.R
+++ tests/testthat/test-basis_functions.R
@@ -3,3 +3,9 @@ test_that("fourier_2d", {
   expect_error(fourier_2d(c(-1,4), c(5,5)))
   expect_error(fourier_2d(c(1,4), c(15,15,2)))
 })
+
+test_that("polynomial_2d", {
+  expect_error(polynomial_2d(1, c(3,4)))
+  expect_error(polynomial_2d(c(-1,4), c(5,5)))
+  expect_error(polynomial_2d(c(1,4), c(15,15,2)))
+})
