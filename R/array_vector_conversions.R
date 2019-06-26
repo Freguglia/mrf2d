cc0ce74ca0dfe75de9d5eb83a46016d3e8e9590e
Freguglia <victorfreguglia@gmail.com>
2019-06-25 21:58:57 -0300

    Started working on "dif" family.  * validity check is done.  * conversions still have to be implemented.  * tests need to be added.

--- R/array_vector_conversions.R
+++ R/array_vector_conversions.R
@@ -1,15 +1,26 @@
 # Checks if an array is a valid potential definition.
 is_valid_array <- function(arr, family){
   n_r <- dim(arr)[3]
-  C <- dim(arr)[1]
-  if(dim(arr)[2] != C) {
+  C <- dim(arr)[1] - 1
+  if(dim(arr)[2] != (C+1)) {
     stop("Invalid array: number of rows and columns differ")}
 
   if(family == "onepar"){
     b <- arr[1,1,1]
-    diags_ok <- all(apply(arr, MARGIN = 3, function(x) all(diag(x) == b)))
-    rest_ok <- all(apply(arr, MARGIN = 3, function(x) all(x[row(x)!=col(x)] == -b)))
+    diags_ok <- all(apply(arr, MARGIN = 3, function(m) all(diag(m) == b)))
+    rest_ok <- all(apply(arr, MARGIN = 3, function(m) all(m[row(m)!=col(m)] == -b)))
     return(diags_ok & rest_ok)
+  } else if(family == "dif") {
+    all(apply(arr, MARGIN = 3, function(m){
+      #Check if matrix is valid:
+      # Are potentials equal for each difference?
+      one_by_dif <- sapply(-C:C, function(v){
+        length(unique(m[row(m)-col(m)==v])) == 1
+      })
+      # Do values sum zero?
+      zero_sum <- abs(sum(m[ ,1]) + sum(m[1, 2:(C+1)])) < 10^-6
+      return(all(one_by_dif) & zero_sum)
+    }))
   } else {
     stop("'", family, "' is not an implemented family.")
   }
@@ -18,8 +29,12 @@ is_valid_array <- function(arr, family){
 # Converts vector to appropriate array of potentials.
 vec_to_array <- function(vec, family, C, n_R){
   if(family == "onepar") {
-    if(length(vec) != 1) {stop("'vec' must have length 1 for family 'onepar'.")}
+    if(length(vec) != 1) { stop("'vec' must have length 1 for family 'onepar'.") }
     return(array(diag(C+1)*2 - 1, dim = c(C+1, C+1, n_R))*vec)
+  } else if(family == "dif") {
+    if(length(vec) != n_R*2*C) {
+      stop("'vec' must have length n_R*2*C for family 'dif'.")}
+
   } else {
     stop("'", family, "' is not an implemented family.")
   }
