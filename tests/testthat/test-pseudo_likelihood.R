366d11552f1d0c64fac6076cd31d0d16e87be6e3
Freguglia <victorfreguglia@gmail.com>
2019-06-27 17:28:47 -0300

    Added data to use in tests and examples. Tests are updated to use it already.

--- tests/testthat/test-pseudo_likelihood.R
+++ tests/testthat/test-pseudo_likelihood.R
@@ -1,18 +1,15 @@
 test_that("Pseudo-likelihood computing is correct", {
-  theta <- array(diag(3)*2 -1, dim = c(3,3,2))*0.7
-  potts <- new("mrfi", Rmat = diag(2), n_neis = 2)
-  set.seed(1)
-  Z <- rmrf2d(c(30,30), potts, theta)
-  expect_equal(pl_mrf2d(matrix(c(0,0,2,2), nrow = 2), potts, theta), log(exp(0)/(exp(0)+exp(0)+exp(-1.4)))*4, tolerance = 10^-6)
-  expect_equal(pl_mrf2d(Z, potts, theta*0), -log(3)*30*30, tolerance = 10^-6)
+  expect_equal(pl_mrf2d(matrix(c(0,0,2,2), nrow = 2), ipotts, theta_potts), log(exp(0)/(exp(0)+exp(0)+exp(-1.4)))*4, tolerance = 10^-6)
+  expect_equal(pl_mrf2d(Z_potts, ipotts, theta_potts*0),
+               -log(3)*prod(dim(Z_potts)), tolerance = 10^-6)
 
   # 'onepar' maximum pseudo-likelihood fitting
-  onepar <- fit_pl(Z, potts, family = "onepar")
+  onepar <- fit_pl(Z_potts[1:30, 1:30], ipotts, family = "onepar")
   expect_setequal(names(onepar), c("theta", "value"))
-  onepar <- fit_pl(Z, potts, optim_args = list(method = "CG"), return_optim = TRUE)
+  onepar <- fit_pl(Z_potts[1:30, 1:30], ipotts, optim_args = list(method = "CG"), return_optim = TRUE)
   expect_true(all(c("theta", "value", "opt.convergence") %in% names(onepar)))
 
   # 'dif' maximum pseudo-likelihood fitting
-  dif <- fit_pl(Z, potts, family = "dif")
+  dif <- fit_pl(Z_potts[1:30, 1:30], ipotts, family = "dif")
   expect_setequal(names(dif), c("theta", "value"))
 })
--- tests/testthat/test-rmrf2d.R
+++ tests/testthat/test-rmrf2d.R
@@ -1,13 +1,10 @@
 test_that("mrf Gibbs Sampler", {
-  theta <- array(diag(3)*2 -1, dim = c(3,3,2))*0.7
-  potts <- new("mrfi", Rmat = diag(2), n_neis = 2)
-
-  expect_error(rmrf2d("string", potts, theta))
-  expect_error(rmrf2d(c(20,20,20), potts, theta))
-  expect_error(rmrf2d(c(-30,30), potts, theta))
+  expect_error(rmrf2d("string", ipotts, theta_potts))
+  expect_error(rmrf2d(c(20,20,20), ipotts, theta_potts))
+  expect_error(rmrf2d(c(-30,30), ipotts, theta_potts))
 
   set.seed(1)
-  Z <- rmrf2d(c(30,30), potts, theta)
+  Z <- rmrf2d(c(30,30), ipotts, theta_potts)
 
   expect_type(Z, "integer")
   expect_true(is.matrix(Z))
