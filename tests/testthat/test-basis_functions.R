332776a3ec17b6b93e6798834e56207f80398cae
Freguglia <victorfreguglia@gmail.com>
2019-06-27 17:57:04 -0300

    Added more tests to basis functions creation.

--- tests/testthat/test-basis_functions.R
+++ tests/testthat/test-basis_functions.R
@@ -1,11 +1,15 @@
 test_that("fourier_2d", {
-  expect_error(fourier_2d(1, c(3,4)))
-  expect_error(fourier_2d(c(-1,4), c(5,5)))
-  expect_error(fourier_2d(c(1,4), c(15,15,2)))
+  expect_error(fourier_2d(max_freqs = 1, lattice_dim = c(3,4)))
+  expect_error(fourier_2d(max_freqs = c(-1,4), lattice_dim = c(5,5)))
+  expect_error(fourier_2d(max_freqs = c(1,4), lattice_dim = c(15,15,2)))
+  expect_is(fourier_2d(max_freqs = c(2,2), lattice_dim = c(10,10)), "list")
+  expect_is(fourier_2d(max_freqs = c(2,2), lattice_dim = c(10,10))[[1]], "function")
 })
 
 test_that("polynomial_2d", {
-  expect_error(polynomial_2d(1, c(3,4)))
-  expect_error(polynomial_2d(c(-1,4), c(5,5)))
-  expect_error(polynomial_2d(c(1,4), c(15,15,2)))
+  expect_error(polynomial_2d(poly_deg = 1, lattice_dim = c(3,4)))
+  expect_error(polynomial_2d(poly_deg = c(-1,4), lattice_dim = c(5,5)))
+  expect_error(polynomial_2d(poly_deg = c(1,4), lattice_dim = c(15,15,2)))
+  expect_is(polynomial_2d(poly_deg = c(2,2), lattice_dim = c(10,10)), "list")
+  expect_is(polynomial_2d(poly_deg = c(2,2), lattice_dim = c(10,10))[[1]], "function")
 })
