863c12da0a47272dcd2b37d99b569fbe89089961
Freguglia <victorfreguglia@gmail.com>
2019-06-28 22:30:47 -0300

    Added tests to vector-array conversions.

---
+++ tests/testthat/test-array_vector_conversions.R
@@ -0,0 +1,13 @@
+test_that("vector - array conversions works", {
+  # 'onepar' family
+  expect_identical(vec_to_array(-1, "onepar", C = 2, n_R = 2), theta_potts)
+  expect_identical(array_to_vec(vec_to_array(-1, "onepar", C = 2, n_R = 2), "onepar"), -1)
+  expect_identical(array_to_vec(vec_to_array(-1, "onepar", C = 6, n_R = 4), "onepar"), -1)
+
+  # 'dif' family
+  set.seed(1); v1 <- runif(4*2); v2 <- runif(6*3)
+  expect_identical(array_to_vec(vec_to_array(v1, "dif", C = 2, n_R = 2), "dif"), v1)
+  expect_identical(array_to_vec(vec_to_array(v2, "dif", C = 3, n_R = 3), "dif"), v2)
+  expect_equal(vec_to_array(v1, "dif", 2, 2)[3,1,1], v1[1])
+
+})
