528714c3b468e7687ea0ca775203b42e62862f20
Freguglia <victorfreguglia@gmail.com>
2019-07-01 12:46:27 -0300

    Added sufficient statistics computing (will be used in stochastic approximation).

---
+++ R/suf_stat.R
@@ -0,0 +1,35 @@
+# Converts an array of pairwise counts to a vector of sufficient statistics.
+suf_stat <- function(arr, family){
+  n_R <- dim(arr)[3]
+  C <- dim(arr)[1] - 1
+  if(C == 0) stop("Each slice needs at least two rows/columns.")
+  if(any(arr < 1 & arr > 0)) stop("'arr' must contain counts instead of proportions.")
+
+  if(family == "onepar"){
+    sum((1 - array(diag(C+1), dim = c(C+1, C+1, n_R)))*arr)
+
+  } else if(family == "oneeach"){
+    apply(arr, MARGIN = 3, function(m){
+      sum((1 - diag(C+1))*m)
+    })
+
+  } else if(family == "absdif"){
+    as.vector(apply(arr, MARGIN = 3, function(m) {
+      sapply(c(1:C), function(v) {
+        sum(m[abs(col(m) - row(m)) == v])
+      })
+    }))
+
+  } else if(family == "dif"){
+    as.vector(apply(arr, MARGIN = 3, function(m) {
+      sapply(c(-C:-1,1:C), function(v) {
+        sum(m[col(m) - row(m) == v])
+      })
+    }))
+
+  } else if(family == "free"){
+    as.vector(apply(arr, MARGIN = 3, function(m){
+      return(as.vector(m)[-1])
+    }))
+  }
+}
---
+++ tests/testthat/test-suf_stat.R
@@ -0,0 +1,8 @@
+test_that("sufficient statistics computing works", {
+  pc <- table_relative_3d(Z_potts, diag(2), C = 2, FALSE)
+  expect_equal(length(suf_stat(pc, "onepar")), 1)
+  expect_equal(length(suf_stat(pc, "oneeach")), 2)
+  expect_equal(length(suf_stat(pc, "absdif")), 2*2)
+  expect_equal(length(suf_stat(pc, "dif")), 2*4)
+  expect_equal(length(suf_stat(pc, "free")), 2*(3*3 -1))
+})
