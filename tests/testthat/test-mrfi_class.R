9bb1d2d153705a818a74b853d486072039e28fcb
Freguglia <victorfreguglia@gmail.com>
2019-07-01 11:51:53 -0300

    removed irrelevant tests.

--- tests/testthat/test-mrfi_class.R
+++ tests/testthat/test-mrfi_class.R
@@ -2,6 +2,4 @@ test_that("mrfi class works", {
   expect_is(ipotts, "mrfi")
   expect_is(plot(ipotts), "ggplot")
   expect_is(plot(ipotts, no_axis = TRUE), "ggplot")
-  expect_warning(show(ipotts), regexp = NA)
-  expect_warning(print(ipotts), regexp = NA)
 })
