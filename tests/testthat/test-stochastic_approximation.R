15c98c025efec2221fdb4e2e1f756051d1686cfb
Freguglia <victorfreguglia@gmail.com>
2019-07-05 16:55:22 -0300

    Added Stochastic Approximation tests.

---
+++ tests/testthat/test-stochastic_approximation.R
@@ -0,0 +1,5 @@
+test_that("stochastic approximation works", {
+  expect_error(fit_sa(Z_potts, mrfi(), family = "onepar", init = c(1,2), gamma_seq = 1:0))
+  expect_is(fit_sa(Z_potts, mrfi(), family = "onepar", gamma_seq = 1:0), "list")
+  expect_true(is_valid_array(fit_sa(Z_potts, mrfi(), family = "dif", init = rep(-1, 4*2), gamma_seq = seq(1,0,-0.05))$theta, "dif"))
+})
