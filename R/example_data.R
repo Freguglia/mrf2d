206d2ad303d755797dd6fc10cc563cfe45bd5dda
Freguglia <victorfreguglia@gmail.com>
2019-07-25 13:57:31 -0300

    Documented the example objects in the package.[ci skip]

--- R/example_data.R
+++ R/example_data.R
@@ -1,7 +1,21 @@
 #' @name Z_potts
 #' @title Example objects from `mrf2d`
-#' @docType data
 #' @author Victor Freguglia
+#'
+#' @description `Z_potts` and `theta_potts` are example objects for `mrf2d`.
+#'
+#' `Z_potts` is a `matrix` object containing an observed lattice of a 3 color
+#' (C = 2) Potts model.
+#'
+#' `theta_potts` is the parameter array used to sample it,
+#' it consists of a configuration with one parameter (-1.0) and two relative
+#' positions (to be used with a nearest-neighbor structure).
+#'
+#' @examples
+#' theta_potts
+#' dplot(Z_potts)
+#'
+#' @docType data
 NULL
 
 #' @name theta_potts
