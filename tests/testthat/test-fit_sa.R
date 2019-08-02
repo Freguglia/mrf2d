bdf6ff8b5a215261f362a90e94cfde2dbbdf5cdb
Freguglia <victorfreguglia@gmail.com>
2019-08-02 13:52:55 -0300

    renamed stochastic approximation test files.

--- tests/testthat/test-fit_sa.R
+++ tests/testthat/test-fit_sa.R
@@ -1,4 +1,4 @@
-test_that("stochastic approximation works", {
+test_that("fit_sa works", {
   expect_error(fit_sa(Z_potts, mrfi(), family = "onepar", init = c(1,2), gamma_seq = 1:0))
   expect_is(fit_sa(Z_potts, mrfi(), family = "onepar", gamma_seq = 1:0), "list")
   expect_true(is_valid_array(fit_sa(Z_potts, mrfi(), family = "dif", init = rep(-1, 4*2), gamma_seq = seq(1,0,-0.05))$theta, "dif"))
