488ac517934fc03b3edba7ca266e5382fe5666cd
Freguglia <victorfreguglia@gmail.com>
2019-08-19 15:37:10 -0300

    Stopped exporting fit_icm

--- R/fit_icm.R
+++ R/fit_icm.R
@@ -1,16 +1,16 @@
-#' @name fit_icm
-#' @author Victor Freguglia
-#' @title Iterated Conditional Modes algorithm for image restoration
-#'
-#' @description Computes maximum probability configuration for a Markov random
-#'  with corrupted pixel values.
-#'
-#' @inheritParams pl_mrf2d
-#' @param n_cycles The number of times each pixel is updated.
-#' @param error_prob The probability a pixel's value is corrupted.
-#'
-#' @return A `matrix` with the resulting configuration.
-#' @export
+# @name fit_icm
+# @author Victor Freguglia
+# @title Iterated Conditional Modes algorithm for image restoration
+#
+# @description Computes maximum probability configuration for a Markov random
+#  with corrupted pixel values.
+#
+# @inheritParams pl_mrf2d
+# @param n_cycles The number of times each pixel is updated.
+# @param error_prob The probability a pixel's value is corrupted.
+#
+# @return A `matrix` with the resulting configuration.
+#' @keywords internal
 fit_icm <- function(init_Z, mrfi, theta, error_prob, cycles = 10){
   if(error_prob >= 1 | error_prob <= 0) {
     stop("Error probability 'error_prob' must be between 0 and 1.")}
