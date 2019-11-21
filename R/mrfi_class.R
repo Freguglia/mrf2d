4848a79b826a6475f23a7ffb964dcd3755267ab8
Freguglia <victorfreguglia@gmail.com>
2019-11-20 21:43:50 -0300

    Minor fix to inconsistencies of mrfi() using integer or double types.

--- R/mrfi_class.R
+++ R/mrfi_class.R
@@ -104,7 +104,7 @@ mrfi <- function(max_norm = 1, norm_type = "1", positions = NULL){
     df <- df[apply(df, MARGIN = 1,
                    function(m) norm(matrix(m), type = norm_type)) <= max_norm,]
   } else {
-    df <- data.frame(x = c(0,0), y = c(0,0))
+    df <- data.frame(x = c(0L,0L), y = c(0L,0L))
   }
 
   if(!is.null(positions)){
--- tests/testthat/test-mrfi_class.R
+++ tests/testthat/test-mrfi_class.R
@@ -7,7 +7,7 @@ test_that("mrfi class works", {
   expect_error(mrfi(max_norm = -1))
   expect_error(mrfi(positions = c(1,0)))
   expect_error(mrfi(positions = list(c("1","0"))))
-  expect_identical(mrfi(positions = list(c(3,3)))@Rmat, rbind(diag(2), c(3,3)))
+  expect_equal(mrfi(positions = list(c(3,3)))@Rmat, rbind(diag(2), c(3,3)))
 
   ## Subsetting and conversion
   expect_error(mrfi(1)[3])
@@ -18,6 +18,9 @@ test_that("mrfi class works", {
   # '+'
   expect_error(mrfi(1) + c(1,0,0))
   expect_error(mrfi(1) + 2)
+  expect_error(mrfi(1) + c(1.2, 0))
   expect_identical(mrfi(1, positions = list(c(2,0))), mrfi(1) + c(2,0))
   expect_identical(mrfi(1, positions = list(c(2,0))), mrfi(1) + mrfi(0, positions = list(c(2,0))))
+  expect_identical(mrfi(1), mrfi(1) + mrfi(1))
+  expect_identical(mrfi(1), mrfi(1) + c(-1,0))
 })
