d1a43c55616197442ddcd6fecc36f1742e6d92a6
Freguglia <victorfreguglia@gmail.com>
2019-11-22 21:34:40 -0300

    Separated mrfi class tests into multiple categories.

--- tests/testthat/test-mrfi_class.R
+++ tests/testthat/test-mrfi_class.R
@@ -1,22 +1,25 @@
-test_that("mrfi class works", {
+test_that("mrfi plotting works", {
   expect_is(mrfi(), "mrfi")
   expect_is(plot(mrfi()), "ggplot")
   expect_is(plot(mrfi(), no_axis = TRUE), "ggplot")
+})
 
-  ## Creator tests
+test_that("mrfi creation works", {
   expect_error(mrfi(max_norm = -1))
   expect_error(mrfi(positions = c(1,0)))
   expect_error(mrfi(positions = list(c("1","0"))))
   expect_identical(mrfi(1), mrfi(1, positions = list()))
   expect_equal(mrfi(positions = list(c(3,3)))@Rmat, rbind(diag(2), c(3,3)))
+})
 
-  ## Subsetting and conversion
+test_that("mrfi subsetting and conversion", {
   expect_error(mrfi(1)[3])
   expect_error(mrfi(1)[[3]])
   expect_identical(mrfi(1), mrfi(1)[1:2])
   expect_identical(as.list(mrfi(1)), mrfi(1)[[1:2]])
+})
 
-  # '+'
+test_that("mrfi + and - operators work", {
   expect_error(mrfi(1) + c(1,0,0))
   expect_error(mrfi(1) + 2)
   expect_error(mrfi(1) + c(1.2, 0))
@@ -25,7 +28,6 @@ test_that("mrfi class works", {
   expect_identical(mrfi(1), mrfi(1) + mrfi(1))
   expect_identical(mrfi(1), mrfi(1) + c(-1,0))
 
-  # '-'
   expect_error(mrfi(1) - c(1,0,0))
   expect_error(mrfi(1) - 2)
   expect_error(mrfi(1) - c(1.2,0))
