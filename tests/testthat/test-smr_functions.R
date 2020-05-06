29b5eee8bfec694129666bf8d364cb28ac5886a7
Freguglia <victorfreguglia@gmail.com>
2020-05-06 13:44:16 -0300

    tests for expand_array.

--- tests/testthat/test-smr_functions.R
+++ tests/testthat/test-smr_functions.R
@@ -14,3 +14,13 @@ test_that("smr_stat works", {
   expect_length(smr_stat(Z_potts, mrfi(), "free"), 2*(3^2 - 1))
 })
 
+test_that("expand_array", {
+  expect_is(expand_array(0.95, family = "onepar", mrfi(1), C = 1), "array")
+  expect_is(expand_array(c(0.1, 0.2), family = "oneeach", mrfi(1), C = 1), "array")
+
+  expect_equivalent(1:2, smr_array(expand_array(1:2, "oneeach", mrfi(1), 2), "oneeach"))
+  expect_equivalent(1:4, smr_array(expand_array(1:4, "absdif", mrfi(1), 2), "absdif"))
+  expect_equivalent(1:8, smr_array(expand_array(1:8, "dif", mrfi(1), 2), "dif"))
+  expect_equivalent(1:16, smr_array(expand_array(1:16, "free", mrfi(1), 2), "free"))
+})
+
