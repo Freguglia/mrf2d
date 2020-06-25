3e0a7d127bd37a2733ddbaa1c45a9191d6b3a5a8
Freguglia <victorfreguglia@gmail.com>
2020-06-24 21:03:55 -0300

    More tests for mrfout methods.

--- tests/testthat/test-mrfout.R
+++ tests/testthat/test-mrfout.R
@@ -1,23 +1,32 @@
-test_that("mrfout prints correctly", {
+test_that("mrfout methods work", {
   # Print
   a <- fit_pl(Z_potts[1:30, 1:30], mrfi(1))
   prnt <- capture.output(print(a))
   expect_true(sum(grepl("Pseudolikelihood", prnt)) > 0)
 
   # Summary
+  a <- fit_pl(Z_potts[1:30, 1:30], mrfi(1), family = "onepar")
+  prnt <- capture.output(summary(a))
+  expect_true(sum(grepl("Interaction", prnt)) > 0)
+  expect_is(plot(a), "ggplot")
+
   a <- fit_pl(Z_potts[1:30, 1:30], mrfi(1), family = "oneeach")
   prnt <- capture.output(summary(a))
   expect_true(sum(grepl("Interaction", prnt)) > 0)
+  expect_is(plot(a), "ggplot")
 
   a <- fit_pl(Z_potts[1:30, 1:30], mrfi(1), family = "absdif")
   prnt <- capture.output(summary(a))
   expect_true(sum(grepl("Interaction", prnt)) > 0)
+  expect_is(plot(a), "ggplot")
 
   a <- fit_pl(Z_potts[1:30, 1:30], mrfi(1), family = "dif")
   prnt <- capture.output(summary(a))
   expect_true(sum(grepl("Interaction", prnt)) > 0)
+  expect_is(plot(a), "ggplot")
 
   a <- fit_pl(Z_potts[1:30, 1:30], mrfi(1), family = "free")
   prnt <- capture.output(summary(a))
   expect_true(sum(grepl("Interaction", prnt)) > 0)
+  expect_is(plot(a), "ggplot")
 })
